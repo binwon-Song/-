ec809bc12467b7583d148aed28363eb7d192b93e
Binwon <binwon236@gmail.com>
2022-10-14 18:39:23 +0900

    Revert "6th week"
    This reverts commit 04b15d33f9af92455cd19f6f168394dacc1904d3.

--- src/6주차_개인과제/task9.java
+++
@@ -1,9 +0,0 @@
-import java.util.StringTokenizer;
-public class task9 {
-    public static void main(String[] args) {
-        StringTokenizer st = new StringTokenizer("홍길동/장화/홍련/콩쥐/팥쥐", "/");
-        while (st.hasMoreTokens())
-        System.out.println(st.nextToken());
-        }        
-}
-
